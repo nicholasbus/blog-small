1488027f514fb2dbd9856709466790f7d5c044c7
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-14 20:56:38 -0500

    created post component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+    },
   },
   variants: {
     extend: {},
