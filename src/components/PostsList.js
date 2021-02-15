1f4eb891fe177649f97409d3419a73aaf1507bff
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-14 20:06:04 -0500

    made 3 vertical components

---
+++ src/components/PostsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostsList = () => {
+    return (
+        <div className="bg-blue-200 flex-grow shadow-2xl-inner flex-col">
+            <h1>Posts List</h1>
+        </div>
+    )
+}
+
+export default PostsList
---
+++ src/components/RightPanel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RightPanel = () => {
+    return (
+        <div className="w-48 z-10 shadow-2xl flex-col">
+            <p>Right Panel</p>
+            <p>Right Panel</p>
+            <p>Right Panel</p>
+            <p>Right Panel</p>
+        </div>
+    )
+}
+
+export default RightPanel
